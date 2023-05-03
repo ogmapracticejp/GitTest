bb6f00e35191ae54452de69c6d38237a909289e3
hanr3 <hanr3@192.168.1.183>
2023-05-03 15:53:13 +0900

    han

---
+++ GitTest/src/GitSample_han.java
@@ -0,0 +1,13 @@
+
+public class GitSample_han {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+System.out.println("lucky3");
+System.out.println("wang3");
+
+System.out.println("lucky666");
+System.out.println("wang");
+	}
+
+}

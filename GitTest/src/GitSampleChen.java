fd4d91113ac3341c689eabecfd2b69b433fa636e
fueli <fueli@fuelien>
2023-05-03 15:47:25 +0900

    0503Chen

---
+++ GitTest/src/GitSampleChen.java
@@ -0,0 +1,12 @@
+
+public class GitSampleChen {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+System.out.println("lucky5");
+System.out.println("wang5");
+
+System.out.println("wang6");
+	}
+
+}
